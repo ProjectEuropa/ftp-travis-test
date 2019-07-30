01e58402f724a42172a98843f377b67a7f77abd0
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-07-30 21:43:49 +0900

    fix delete dirname

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 files=./dist/*
 for f in $files
   do
-	  echo "Uploading $f";
+    f="$(basename -- $files)"
+    echo "Uploading $f";
     curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T $f ftp://$FTP_HOST$FTP_REMOTE_ROOT/$f \;
   done
