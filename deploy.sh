f7838637e4af9a54d68f99f686bf810c68b5b00d
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-07-30 18:38:03 +0900

    set

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 files=./dist/*
 for f in $files
   do
-	  echo "Uploading ";
-	  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_HOST$FTP_REMOTE_ROOT/$f
+	  echo "Uploading $f";
+    curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T $f ftp://$FTP_HOST$FTP_REMOTE_ROOT/$f \;
   done
