18d31f4aa8f5396bd8dca8660e9e49f286f1be7f
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-07-30 16:00:57 +0900

    feat: set from ftp deploy

--- ftp-deploy.js
+++ ftp-deploy.js
@@ -1,18 +1,44 @@
-var FtpDeploy = require("ftp-deploy");
-var ftpDeploy = new FtpDeploy();
-require('dotenv').config({path: __dirname + '/.env'})
+var fs = require("fs");
+var FtpClient = require("ftp-deploy");
 
-var config = {
-  user: process.env.FTP_USER,
-  password: process.env.FTP_PASSWORD,
-  host: process.env.FTP_HOST,
-  localRoot: "dist/",
-  remoteRoot: process.env.FTP_REMOTE_ROOT,
-  include: ["*"],
-  deleteRemote: true,
-  forcePasv: false
-};
+console.log("Inside ftpUpload.js");
+if (process === null) {
+  console.log("process is null");
+} else {
+  uploadToFTP();
+}
+function getFiles(dir, files_) {
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files) {
+    var name = dir + "/" + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files_);
+    } else {
+      files_.push({ full_path: name, rel_path: files[i] });
+    }
+  }
+  return files_;
+}
 
-ftpDeploy.deploy(config)
-    .then(res => console.log('finished:', res))
-    .catch(err => console.log(err))
+function uploadToFTP(files) {
+  var ftp = new FtpClient();
+  var ftpConfig = getConfiguration();
+
+  ftp.deploy(ftpConfig, function(err, fileName) {
+    if (err) {
+      console.log("error " + err);
+    } else {
+      console.log("Completed uploading");
+    }
+  });
+}
+function getConfiguration() {
+  return {
+    host: process.env.FTP_HOST,
+    username: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    localRoot: "dist",
+    remoteRoot: process.env.FTP_REMOTE_ROOT
+  };
+}
