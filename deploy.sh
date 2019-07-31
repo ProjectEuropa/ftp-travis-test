1ccb7424707cd0d05e49dc43209783cefdadfa9e
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-07-31 18:06:41 +0900

    set lftp

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-files=*
-for f in $files
-  do
-    echo "Uploading $f";
-    curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T $f sftp://$FTP_HOST$FTP_REMOTE_ROOT/$f \;
-  done
+lftp -u $FTP_USER,$FTP_PASSWORD -e "mirror -Rev -X dist/. $FTP_REMOTE_ROOT." $FTP_HOST
+
