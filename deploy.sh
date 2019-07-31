2b307cf5c7659f4fe84e9fd7070a9e5609865798
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-07-31 21:37:20 +0900

    set ssl no

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-lftp -u $FTP_USER,$FTP_PASSWORD -e "mirror -Rev dist/. $FTP_REMOTE_ROOT/." $FTP_HOST
+lftp -u $FTP_USER,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror -Rev dist/. $FTP_REMOTE_ROOT/. $FTP_HOST; quit"
 
