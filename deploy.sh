8e2e0b908b898d4df8a6cd37b8c5cd8c09794c0b
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-01 10:36:09 +0900

    set

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST:21; mirror -eRv dist /$FTP_REMOTE_ROOT; quit;"
+lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST:21; mirror --Rev dist/. /$FTP_REMOTE_ROOT/.; quit;"
