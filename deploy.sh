f137c42ba1ba7b7e316ae4acc1e11454e34c5548
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-01 10:17:12 +0900

    set in all deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-lftp -f setting.conf
+lftp -f "
+set ssl:verify-certificate no
+set ftp:ssl-allow no
+set dns:order 'inet'
+open $FTP_USER:$FTP_PASSWORD@$FTP_HOST
+mirror -v -e -R dist/ /$FTP_REMOTE_ROOT
+"
