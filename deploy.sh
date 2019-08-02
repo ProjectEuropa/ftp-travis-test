ad0d8a2561b2c09bc84e94f9c8a39f5f16cd46e7
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-02 09:59:54 +0900

    delete -

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-- lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror -R dist/index.html /$FTP_REMOTE_ROOT/ --parallel=10"
+lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror -R dist/index.html /$FTP_REMOTE_ROOT/ --parallel=10"
