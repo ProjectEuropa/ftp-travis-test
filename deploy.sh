10edccaf61e4040d466d8a0c9bb759d47503c001
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-02 11:23:59 +0900

    feat add dist./

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lftp -d -c "set ftp:use-mdtm off;set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror --ignore-time -R dist /$FTP_REMOTE_ROOT/ --parallel=20"
+lftp -d -c "set ftp:use-mdtm off;set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror --ignore-time -R dist/. /$FTP_REMOTE_ROOT/ --parallel=20"
