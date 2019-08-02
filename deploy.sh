0b05205a2f674ab3718e1b60bcefce10007efc48
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-02 11:20:09 +0900

    set ignore

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lftp -d -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror -X dist/nuxt -R dist /$FTP_REMOTE_ROOT/ --parallel=10"
+lftp -d -c "set ftp:use-mdtm off;set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror --ignore-time -R dist /$FTP_REMOTE_ROOT/ --parallel=20"
