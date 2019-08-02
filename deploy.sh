99a4230be2e64a34fdb950f7dc6c63992f96aabd
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-02 09:56:49 +0900

    set only index.html

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST:21; mirror -Rev dist/. /$FTP_REMOTE_ROOT/.; quit;"
+- lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror -R dist/index.html /$FTP_REMOTE_ROOT/ --parallel=10"
