e320bc754bf3e81cd8fb909939a87c812fc13059
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-08-02 10:43:29 +0900

    set dist

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror -R -X dist/nuxt dist/. /$FTP_REMOTE_ROOT/ --parallel=10"
+lftp -c "set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST; mirror -R dist /$FTP_REMOTE_ROOT/ --parallel=10"
